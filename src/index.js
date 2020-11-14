40f50cd976bfeb3a7bd82d457f14533dc61f02d6
KD77 <kalid.diriye@gmail.com>
2020-11-14 19:53:48 +0100

    Added quiz file and some basic code for the quiz

--- src/index.js
+++ src/index.js
@@ -3,9 +3,109 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-import confetti from 'canvas-confetti';
+import Quizz from './modules/quizz.js'
 
-confetti.create(document.getElementById('canvas'), {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+const content = document.getElementById("content")
+
+const linkD = document.getElementById("linkD")
+const linkE = document.getElementById("linkE")
+const linkF = document.getElementById("linkF")
+const userId = document.getElementById("userId")
+
+let nextUrl = null;
+
+
+/**
+ * Update the webpage with some content.
+ */
+function updateContent(str) {
+  content.innerHTML = str
+}
+
+
+
+/**
+* Use async await and do a GET request
+*/
+linkD.addEventListener("click", () => {
+  let status = "Clicked D"
+
+  updateContent(status)
+  console.log(status)
+
+  Quizz.getFirstQuestion()
+  .then(response => {
+      console.log(response)
+
+      response.json().then((data) => {
+          updateContent(data.question);
+         
+          console.log(JSON.stringify(data, null, 4))
+
+          nextUrl = data.nextURL
+      });
+  })
+  .catch((err) => {
+      console.log('Fetch Error :-S', err)
+  });
+})
+
+
+
+/**
+* Use async await and do a POST request
+*/
+linkE.addEventListener("click", () => {
+  let status = "Clicked E"
+  let id = userId.value
+  let body = {
+      answer: id
+  }
+
+  updateContent(status + id)
+  console.log(status + id)
+
+  Quizz.sendQuestionResponsePost(nextUrl, body)
+  .then(response => {
+      console.log(response)
+
+      response.json().then((data) => {
+          updateContent(data.message)
+          console.log(JSON.stringify(data, null, 4))
+
+          nextUrl = data.nextURL
+      });
+  })
+  .catch((err) => {
+      console.log('Fetch Error :-S', err)
+  });
+})
+
+
+
+/**
+* Use async await and do a GET request
+*/
+linkF.addEventListener("click", () => {
+  let status = "Clicked F"
+  let id = userId.value
+
+  updateContent(status + id)
+  console.log(status + id)
+
+  Quizz.getQuestion(nextUrl)
+  .then(response => {
+      console.log(response)
+
+      response.json().then((data) => {
+          updateContent(JSON.stringify(data, null, 4))
+          console.log(data)
+
+          nextUrl = data.nextURL
+      });
+  })
+  .catch((err) => {
+      console.log('Fetch Error :-S', err)
+  });
+})
+
