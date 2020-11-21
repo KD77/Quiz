8ba6632b378b37bd0dddc3f39350a4ddf31ffe10
KD77 <kalid.diriye@gmail.com>
2020-11-21 17:13:57 +0100

    basic code for the quiz game

--- quiz/src/index.js
+++ quiz/src/index.js
@@ -1,11 +1,101 @@
+import Quizz from './modules/quizz'
+
+
+const start = document.getElementById("start")
+const enter = document.getElementById("enter")
+const next = document.getElementById("next")
+const userId = document.getElementById("userId")
+
+let nextUrl = null;
+
+
 /**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+ * Update the webpage with some content.
+ */
+function updateContent(str) {
+    content.innerHTML = str
+}
 
-import confetti from 'canvas-confetti';
 
-confetti.create(document.getElementById('canvas'), {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+
+start.addEventListener("click", () => {
+    let status = "Clicked D"
+
+    updateContent(status)
+    console.log(status)
+
+    Quizz.getFirstQuestion()
+    .then(response => {
+        console.log(response)
+
+        response.json().then((data) => {
+            updateContent(JSON.stringify(data, null, 4))
+            console.log(data)
+
+            nextUrl = data.nextURL
+        });
+    })
+    .catch((err) => {
+        console.log('Fetch Error :-S', err)
+    });
+})
+
+
+
+/**
+ * Use async await and do a POST request
+ */
+ enter.addEventListener("click", () => {
+    let status = "Clicked E"
+    let id = userId.value
+    let body = {
+        answer: id
+    }
+
+    updateContent(status + id)
+    console.log(status + id)
+
+    Quizz.sendQuestionResponsePost(nextUrl, body)
+    .then(response => {
+        console.log(response)
+
+        response.json().then((data) => {
+            updateContent(JSON.stringify(data, null, 4))
+            console.log(data)
+
+            nextUrl = data.nextURL
+        });
+    })
+    .catch((err) => {
+        console.log('Fetch Error :-S', err)
+    });
+})
+
+
+
+/**
+ * Use async await and do a GET request
+ */
+next.addEventListener("click", () => {
+    let status = "Clicked F"
+    let id = userId.value
+
+    updateContent(status + id)
+    console.log(status + id)
+
+    Quizz.getQuestion(nextUrl)
+    .then(response => {
+        console.log(response)
+
+        response.json().then((data) => {
+            updateContent(JSON.stringify(data, null, 4))
+            console.log(data)
+
+            nextUrl = data.nextURL
+        });
+    })
+    .catch((err) => {
+        console.log('Fetch Error :-S', err)
+    });
+})
+
---
+++ quiz/src/modules/quizz.js
@@ -0,0 +1,96 @@
+/**
+ * Sample module to use for Quizz using async await
+ */
+async function getFirstQuestion() {
+  const url="http://courselab.lnu.se/question/1"
+  const response = await fetch(url)
+  let data
+
+  if (!response.ok) {
+      let data = await response.json()
+
+      console.log(response)
+      console.log(JSON.stringify(data, null, 4))
+
+      throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  console.log(response)
+  // response.json().then((data) => {
+  //     console.log(JSON.stringify(data, null, 4))
+  // })
+  // data = await response.json()
+  // console.log(JSON.stringify(data, null, 4))
+
+  return response
+};
+
+
+
+async function sendQuestionResponsePost(url, body) {
+  const data = {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json"
+      },
+      body: JSON.stringify(body)
+  }
+  const response = await fetch(url, data)
+
+  if (!response.ok) {
+      let data = await response.json()
+
+      console.log(response)
+      console.log(JSON.stringify(data, null, 4))
+
+      throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  console.log(response)
+  // response.json().then((data) => {
+  //     console.log(JSON.stringify(data, null, 4))
+  // })
+  // data = await response.json()
+  // console.log(JSON.stringify(data, null, 4))
+
+  return response
+};
+
+
+
+async function getQuestion(url) {
+  const data = {
+      method: "GET",
+  }
+
+  console.log(url)
+  console.log(data)
+  const response = await fetch(url, data)
+  console.log(response)
+
+  if (!response.ok) {
+      let data = await response.json()
+
+      console.log(response)
+      console.log(JSON.stringify(data, null, 4))
+
+      throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  console.log(response)
+  // response.json().then((data) => {
+  //     console.log(JSON.stringify(data, null, 4))
+  // })
+  // data = await response.json()
+  // console.log(JSON.stringify(data, null, 4))
+
+  return response
+};
+
+
+
+export default {
+  getFirstQuestion,
+  sendQuestionResponsePost,
+  getQuestion
+};
